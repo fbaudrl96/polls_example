96ef765d69172a447cbacaadae16fec80d3ca7bf
fbaudrl96 <fbaudrl96@gmail.com>
2020-06-03 15:23:22 +0900

    20.06.03 upload

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,25 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
 
+# class Student(models.Model):
+#   choice
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
